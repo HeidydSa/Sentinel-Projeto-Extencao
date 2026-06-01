702c9a03126961f0835b9c27aadd6195d776f358
starzlocker <adolfo.gomes@al.infnet.edu.br>
2026-05-31 21:25:12 -0300

    feat: criação da config do db firestore

--- src/config/db_config.js
+++ src/config/db_config.js
@@ -1,15 +1,7 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyB3coiGzKFnQTFZYR5MKNF36zzzM4yQbRg',
@@ -21,10 +13,8 @@ const firebaseConfig = {
   measurementId: 'G-RDYLSJ782T',
 };
 
-// Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 
-// const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-export default app;
+export default db;
